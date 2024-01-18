f969e3f30a68838220fca9ddb21a474491d7f31a
p4 <arnaudfilippi06@gmail.com>
2024-01-18 15:27:22 +0000

    le controller avance...

--- load_balancer/load_balancer_controller.py
+++ load_balancer/load_balancer_controller.py
@@ -17,11 +17,12 @@ class LoadBalancerController(cmd2.Cmd):
         self.topo = load_topo('topology.json')
         self.sw_name = sw_name
         self.port_in = port_in
+        self.host_connected = [] #port and mac_address
         self.thrift_port = self.topo.get_thrift_port(sw_name)
         self.controller = SimpleSwitchThriftAPI(self.thrift_port)
         self.controller = swap(self.sw_name, 'load_balancer')
 
-    def see_load(self):
+    def see_load(self,args):
         print("Total counter: ")
         self.controller.counter_read('count_in', 0)
         print("\u200B")
@@ -29,7 +30,17 @@ class LoadBalancerController(cmd2.Cmd):
     def add_line_table(self, port_src, mc_address_port_in, port_in):
         self.controller.table_add("port_to_nhop", "set_nhop", [port_src], [mc_address_port_in, port_in])
         print("Line added : port_to_nhop" + str(port_src) + "to" + str(port_in))
-        print("\u200B")    
+        print("\u200B")
+
+    def get_neighbor(self,args):
+        neighbor = self.topo.get_hosts_connected_to(self.sw_name)
+        neighbor_list=[]
+        for i in neighbor:
+            neighbor_list.append([self.topo.get_host_mac(i), self.topo.node_to_node_port_num(self.sw_name,i)])
+        return neighbor_list
+
+    def do_update_neighbor(self,args):
+        neighbor_list = self.get_neighbor(self)
 
 def matches_regex(string, regex):
     return re.match(regex, string) is not None

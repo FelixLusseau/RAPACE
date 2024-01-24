73ff545a98908ec9d36f181270d24f378e6c79d1
p4 <arnaudfilippi06@gmail.com>
2024-01-24 23:41:47 +0000

    clean env

--- network.py
+++ network.py
@@ -10,23 +10,13 @@ def runMininet():
 	# Network definition
 	net.addP4Switch('s1')
 	net.addP4Switch('s2')
-	net.addP4Switch('s3')
-	net.addP4Switch('s4')
-	net.addP4Switch('s5')
-	net.addP4Switch('s6')
-	net.addP4Switch('s4')
-	net.addP4Switch('s5')
 
 	net.addHost('h1')
 	net.addHost('h2')
 
 	net.addLink('h1', 's1')
 	net.addLink('s1', 's2')
-	net.addLink('s2', 's3')
-	net.addLink('s1', 'h1')
-	net.addLink('s3', 's4')
-	net.addLink('s4', 'h2')
-	net.addLink('s2', 's5')
+	net.addLink('s2', 'h2')
 
 	# Assignment strategy
 	net.l2()
